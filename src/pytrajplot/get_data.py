bf6a79a476304925b9b1a84bef4c4420b9c4552f
mizeller <mizeller@student.ethz.ch>
2021-10-19 19:14:16 +0200

    buffer

--- src/pytrajplot/get_data.py
+++ src/pytrajplot/get_data.py
@@ -11,15 +11,16 @@ import pandas as pd
 plot_steps = True
 global_path = os.getcwd() + "/zmichel/21101100_407/lagranto_c/"
 
+
 # REMARK: the plot_info can be differentiated from the other files, because the first line is empty (or just simply by its name of course)
-def read_plot_info():
+def read_plot_info(plot_info_path):
     """Read the pure txt file containing the plot_info to variables for later purposes."""
     if plot_steps:
         print("--- reading plot_info into dict")
 
     # TODO: make path to plot_info file a CLI (command line input), furthermore check
     # if the file exists, otherwise print error message and abort
-    plot_info_path = global_path + "plot_info"
+    # plot_info_path = global_path + "plot_info"
 
     with open(plot_info_path, "r") as f:
         plot_info_tmp = [line.strip() for line in f.readlines()]
@@ -42,16 +43,28 @@ def read_plot_info():
     return plot_info_dict
 
 
-def read_startf():
+def count_unique_origins(data):
+    # print(data["origin"])
+    i = 0
+    for str in data["origin"]:
+        # print(str)
+        if "~" in str:
+            data["origin"][i] = str[: (len(str) - 2)]
+        i += 1
+    # print(data["origin"])
+    return data["origin"].nunique()
+
+
+def read_startf(startf_path):
     if plot_steps:
         print("--- reading startf file")
 
     # TODO: make path to plot_info file a CLI (command line input), furthermore check
     # if the file exists, otherwise print error message and abort
-    startf_file = global_path + "startf_003-033F"  # 'startf_000-033F'
+    # startf_file = global_path + 'startf_000-033F' #"startf_003-033F"
 
-    data = pd.read_csv(
-        startf_file,
+    start_df = pd.read_csv(
+        startf_path,
         skiprows=0,
         skipfooter=0,
         sep=" ",
@@ -62,14 +75,87 @@ def read_startf():
         skipinitialspace=True,
     )
 
-    # print(data)
-    print(data["origin"])
+    unique_origins = count_unique_origins(data=start_df)
+    # print(unique_origins)
+    return start_df
 
-    i = 0
-    for str in data["origin"]:
-        # print(str)
-        if "~" in str:
-            data["origin"][i] = str[: (len(str) - 2)]
-        i += 1
-    print(data["origin"])
-    return data
+
+def read_tra_files():
+    if plot_steps:
+        print("--- reading trajectory file")
+
+    trajectory_file_path = global_path + "tra_geom_000-033F"
+
+    info = pd.read_csv(
+        trajectory_file_path,
+        skiprows=16,
+        skipfooter=0,
+        nrows=2,
+        sep=":",
+        header=None,
+        engine="python",
+        skipinitialspace=True,
+    )
+
+    number_of_trajectories = info[1][0]
+    number_of_times = info[1][1]
+
+    print(
+        f"number of trajecotries = {number_of_trajectories} \nnumber of times = {number_of_times}"
+    )
+
+    header_line = pd.read_csv(
+        trajectory_file_path,
+        skiprows=19,
+        skipfooter=0,
+        nrows=1,
+        sep=" ",
+        header=None,
+        engine="python",
+        skipinitialspace=True,
+    )
+    print(header_line)
+
+    traj_df = pd.read_csv(
+        trajectory_file_path,
+        skiprows=23,
+        skipfooter=0,
+        sep=" ",
+        names=header_line.values.astype(str)[0, :],
+        engine="python",
+        skipinitialspace=True,
+    )
+    print(traj_df.head())
+    traj_df.to_csv("trajectory_data.csv", index=False)
+
+
+def check_input_dir():  # iterate through the input folder containing the trajectorie coordinates
+    if plot_steps:
+        print("--- iterating through input directory")
+
+    plot_info_dict = {}
+    frames = []
+    # start_df = {}
+
+    for filename in os.listdir(global_path):
+        f = os.path.join(global_path, filename)  # add filename to global_path
+        if os.path.isfile(f):  # check if it's a file
+            # print(f)
+            with open(f, "r") as file:
+                first_line = file.readline()
+                print(
+                    f"First line of file {filename}: {first_line} ({len(first_line)})"
+                )
+
+                if len(first_line) == 1:
+                    plot_info_dict = read_plot_info(f)
+
+                # if first_line[:8] == 'LAGRANTO':
+                #     # TODO: parse the trajectory files
+                # else:
+                #     start_df = read_startf(f)
+                #     print('start_df looks like: ', start_df)
+                #     frames.append(start_df)
+                #     plot_info_dict = pd.concat(frames, keys=['long', 'lat', 'z', 'z_type', 'origin'])
+
+    return plot_info_dict
