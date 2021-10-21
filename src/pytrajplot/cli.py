34f330acab849a4123d770595a3ac671684df403
mizeller <mizeller@student.ethz.ch>
2021-10-21 16:55:59 +0200

    refined command line inputs

--- src/pytrajplot/cli.py
+++ src/pytrajplot/cli.py
@@ -16,32 +16,43 @@ from .utils import count_to_log_level
 @click.command()
 @click.argument("dir_path", type=click.Path(exists=True))
 @click.option(
-    "--start-prefix",
-    default="startf_",
-    type=str,
-    help="Prefix for the start files. Default: startf_",
+    "--prefixes",
+    type=(str, str, str),
+    default=("startf_", "tra_geom_", "plot_info"),
+    help="Enter the prefixes for the start, trajectory and plot-info file. (ORDER RELEVANT)",
 )
-@click.option(
-    "--traj-prefix",
-    default="tra_geom_",
-    type=str,
-    help="Prefix for the start files. Default: tra_geom_",
-)
-@click.option(
-    "--info-prefix",
-    default="plot_info",
-    type=str,
-    help="Prefix for the plot info files. Default: plot_info",
-)
-def main(*, dir_path, start_prefix: str, traj_prefix: str, info_prefix: str) -> None:
+# @click.option(
+#     "--start-prefix",
+#     #default="startf_",
+#     type=str,
+#     help="Prefix for the start files. Default: startf_",
+# )
+# @click.option(
+#     "--traj-prefix",
+#     #default="tra_geom_",
+#     type=str,
+#     help="Prefix for the start files. Default: tra_geom_",
+# )
+# @click.option(
+#     "--info-prefix",
+#     #default="plot_info",
+#     type=str,
+#     help="Prefix for the plot info files. Default: plot_info",
+# )
+def main(*, dir_path: str, prefixes: tuple) -> None:
 
-    path = os.getcwd() + "/zmichel/HRES/lagranto_c/"
-    # these prefixes should be cli !
+    # prefix_dict = {
+    #     "start": start_prefix,
+    #     "trajectory": traj_prefix,
+    #     "plot_info": info_prefix,
+    # }
+
+    print(type(dir_path))
 
     prefix_dict = {
-        "start": start_prefix,
-        "trajectory": traj_prefix,
-        "plot_info": info_prefix,
+        "start": prefixes[0],
+        "trajectory": prefixes[1],
+        "plot_info": prefixes[2],
     }
 
     plot_info_dict, trajectory_dict, keys = check_input_dir(
