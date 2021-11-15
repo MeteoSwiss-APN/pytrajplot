eb41667b6d255d9bf05e949d522c06925475cd9b
mizeller <mizeller@student.ethz.ch>
2021-11-15 10:52:54 +0100

    added docstrings

--- src/pytrajplot/cli.py
+++ src/pytrajplot/cli.py
@@ -18,6 +18,19 @@ from .utils import count_to_log_level
 
 
 def interpret_options(start_prefix, traj_prefix, info_prefix, language):
+    """Reformat command line inputs.
+
+    Args:
+        start_prefix (str): Prefix of start files
+        traj_prefix (str):  Prefix of trajectory files
+        info_prefix (str):  Prefix of plot info files
+        language (str):     Language for plot annotations
+
+    Returns:
+        prefix_dict (dict): Dictionary, collecting the prefix-inputs
+        language (str):     Correctly assigned language, based on various inputs
+
+    """
     prefix_dict = {
         "start": start_prefix,
         "trajectory": traj_prefix,
