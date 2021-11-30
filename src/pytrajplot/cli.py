39ef06d1bbfb7d4afaac2c283e55e9fceb2f0840
mizeller <mizeller@student.ethz.ch>
2021-11-30 13:33:31 +0100

    info_prefix --> info_name & optional datatype flag

--- src/pytrajplot/cli.py
+++ src/pytrajplot/cli.py
@@ -9,33 +9,25 @@ from .generate_pdf import generate_pdf
 from .parse_data import check_input_dir
 from .utils import count_to_log_level
 
-# from pytrajplot import plot_map_and_altitude
 
-
-def interpret_options(start_prefix, traj_prefix, info_prefix, language):
+def interpret_options(start_prefix, traj_prefix, info_name, language):
     """Reformat command line inputs.
 
     Args:
-        start_prefix
-                            str       Prefix of start files.
-        traj_prefix
-                            str       Prefix of trajectory files.
-        info_prefix
-                            str       Prefix of info file.
-        language
-                            str       language for plot annotations
+        start_prefix        str       Prefix of start files.
+        traj_prefix         str       Prefix of trajectory files.
+        info_prefix         str       Prefix of info file.
+        language            str       language for plot annotations
 
     Returns:
-        prefix_dict
-                            dict      The various prefixes are stored in this dict.
-        language
-                            str       language for plot annotations after mapping. "en" or "de"
+        prefix_dict         dict      The various prefixes are stored in this dict.
+        language            str       language for plot annotations after mapping. "en" or "de"
 
     """
     prefix_dict = {
         "start": start_prefix,
         "trajectory": traj_prefix,
-        "plot_info": info_prefix,
+        "plot_info": info_name,
     }
 
     if language[0] == "e":
@@ -46,7 +38,6 @@ def interpret_options(start_prefix, traj_prefix, info_prefix, language):
     return prefix_dict, language
 
 
-### DEFINE COMMAND LINE INPUTS
 @click.command()
 @click.argument("input_dir", type=click.Path(exists=True))
 @click.argument("output_dir", type=click.Path(exists=False))
@@ -63,7 +54,7 @@ def interpret_options(start_prefix, traj_prefix, info_prefix, language):
     help="Prefix for the start files. Default: tra_geom_",
 )
 @click.option(
-    "--info-prefix",
+    "--info-name",
     default="plot_info",
     type=str,
     help="Prefix for the plot info files. Default: plot_info",
@@ -101,9 +92,19 @@ def interpret_options(start_prefix, traj_prefix, info_prefix, language):
     default=("centraleurope", "europe", "dynamic", "ch_hd", "ch", "alps"),
     help="Choose domains for map plots. Default: centraleurope, europe, dynamic",
 )
+@click.option(
+    "--datatype",
+    type=click.Choice(
+        ["pdf", "png"],
+        case_sensitive=False,
+    ),
+    multiple=True,
+    default=("png"),
+    help="Choose data type(s) of final result. Default: png",
+)
 def main(
     *,
-    info_prefix: str,
+    info_name: str,
     start_prefix: str,
     traj_prefix: str,
     input_dir: str,
@@ -111,19 +112,20 @@ def main(
     separator: str,
     language: str,
     domain: str,
+    datatype: str,
 ) -> None:
 
     prefix_dict, language = interpret_options(
         start_prefix=start_prefix,
         traj_prefix=traj_prefix,
-        info_prefix=info_prefix,
+        info_name=info_name,
         language=language,
     )
-    print("--- Parsing Input Files")
-    trajectory_dict, plot_info_dict, keys = check_input_dir(
+
+    trajectory_dict, plot_info_dict, _ = check_input_dir(
         input_dir=input_dir, prefix_dict=prefix_dict, separator=separator
     )
-    print("--- Assembling PDF")
+
     generate_pdf(
         trajectory_dict=trajectory_dict,
         plot_info_dict=plot_info_dict,
@@ -131,7 +133,9 @@ def main(
         separator=separator,
         language=language,
         domains=domain,
+        output_types=datatype,
     )
+
     print("--- Done.")
 
     return
