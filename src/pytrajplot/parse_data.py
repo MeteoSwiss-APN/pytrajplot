84e520027da83d3d6dcc13631ff92d0553ce9192
mizeller <mizeller@student.ethz.ch>
2021-12-13 15:27:30 +0100

    removed pandas warning & made parsing pipeline more efficient ~30-40% speed bump

--- src/pytrajplot/parse_data.py
+++ src/pytrajplot/parse_data.py
@@ -1,49 +1,12 @@
 """Util functions to get data."""
 # Standard library
+import datetime
 import os
+from datetime import timedelta
 
 # Third-party
 import numpy as np
 import pandas as pd
-
-pd.options.mode.chained_assignment = None  # default: 'warn'
-
-# Standard library
-import datetime
-from datetime import timedelta
-
-
-def read_plot_info(plot_info_path):
-    """Read the pure txt file containing the plot_info to corresponding variables.
-
-    Args:
-        plot_info_path (str): Path to plot_info file
-
-    Returns:
-        (dict): Dict, containing variables parsed from plot_info file
-
-    """
-    # print("--- reading plot_info into dict")
-
-    with open(plot_info_path, "r") as f:
-        plot_info_tmp = [line.strip() for line in f.readlines()]
-    plot_info = list(
-        filter(str, plot_info_tmp)
-    )  # remove first and last empty line entries
-    plot_info_dict = {
-        "mbt": plot_info[0][44:],  # model base time
-        "model_name": plot_info[1][44:],  # model name
-        "np_long": float(plot_info[2][44:]),  # north pole longitude
-        "np_lat": float(plot_info[3][44:]),  # north pole latitude
-        "start_long": float(plot_info[4][44:]),  # start longitude
-        "start_lat": float(plot_info[5][44:]),  # start latitude
-        "inc_long": float(plot_info[6][44:]),  # increment in longitudinal direction
-        "inc_lat": float(plot_info[7][44:]),  # increment in latitudinal direction
-        "no_pts_long": float(plot_info[8][44:]),  # #points in long. direction
-        "no_pts_lat": float(plot_info[9][44:]),  # #point in lat. direction
-    }
-
-    return plot_info_dict
 
 
 def map_altitudes_and_subplots(unit, unique_start_altitudes, current_altitude):
@@ -148,11 +111,11 @@ def read_startf(startf_path, separator):
                 if (
                     separator in start_df["origin"].loc[i + 1]
                 ):  # if Ǝ separator --> have side trajectories for given origin
-                    start_df["side_traj"].loc[
-                        i : i + 4
+                    start_df.loc[
+                        i : i + 4, "side_traj"
                     ] = 1  # corresponding (5) rows have a 1 (=True) in the side_traj column
-                    start_df["altitude_levels"].loc[
-                        i : i + 4
+                    start_df.loc[
+                        i : i + 4, "altitude_levels"
                     ] = start_df.loc[  # corresponding (5) rows have #alt_levels in the altitude_levels column
                         start_df.origin == start_df["origin"].loc[i], "origin"
                     ].count()
@@ -163,17 +126,15 @@ def read_startf(startf_path, separator):
                         altitude_levels = start_df["altitude_levels"].loc[
                             i
                         ]  # altitude levels for given location
-                        altitude = start_df["z"].loc[i]
                         # add information to unique_altitude_levels_dict
-                        # print(f'{origin}: rows {i} - {i + 5*altitude_levels - 1} correspond to altitude level {altitude} {unit}')
                         unique_altitude_levels_dict[start_df["origin"].loc[i]] = (
                             start_df["z"]
                             .loc[i : (i + 5 * altitude_levels) - 1]
                             .unique()
                         )
 
-                    start_df["subplot_index"].loc[
-                        i : i + 4
+                    start_df.loc[
+                        i : i + 4, "subplot_index"
                     ] = map_altitudes_and_subplots(
                         unit=unit,
                         unique_start_altitudes=unique_altitude_levels_dict[origin],
@@ -181,20 +142,19 @@ def read_startf(startf_path, separator):
                     )
 
                     if unit == "hPa":
-                        start_df["max_start_altitude"].loc[i : i + 4] = np.min(
+                        start_df.loc[i : i + 4, "max_start_altitude"] = np.min(
                             unique_altitude_levels_dict[origin]
                         )
                     if unit == "m":
-                        start_df["max_start_altitude"].loc[i : i + 4] = np.max(
+                        start_df.loc[i : i + 4, "max_start_altitude"] = np.max(
                             unique_altitude_levels_dict[origin]
                         )
-
                     i += 5
 
-                else:
-                    start_df["side_traj"].loc[i : i + 3] = 0
+                else:  # no side trajectories
+                    start_df.loc[i : i + 3, "side_traj"] = 0
 
-                    start_df["altitude_levels"].loc[i : i + 3] = start_df.loc[
+                    start_df.loc[i : i + 3, "altitude_levels"] = start_df.loc[
                         start_df.origin == start_df["origin"].loc[i], "origin"
                     ].count()
 
@@ -209,20 +169,20 @@ def read_startf(startf_path, separator):
                             start_df["z"].loc[i : (i + altitude_levels)].unique()
                         )
 
-                    # print(f'{origin}: row {i} corresponds to altitude level {start_df["z"].loc[i]} {unit}')
                     if unit == "hPa":
-                        start_df["max_start_altitude"].loc[i : i + 3] = np.min(
+                        start_df.loc[i : i + 3, "max_start_altitude"] = np.min(
                             unique_altitude_levels_dict[origin]
                         )
                     if unit == "m":
-                        start_df["max_start_altitude"].loc[i : i + 3] = np.max(
+                        start_df.loc[i : i + 3, "max_start_altitude"] = np.max(
                             unique_altitude_levels_dict[origin]
                         )
 
                     tmp = i
                     while tmp < i + 4:
+
                         # print(f'Calling map_alt_&_suplots for {origin} w/ unit = {unit}, unique start alts = {unique_altitude_levels_dict[origin]} & current_alt = {start_df["z"].loc[tmp]}')
-                        start_df["subplot_index"].loc[tmp] = map_altitudes_and_subplots(
+                        start_df.loc[tmp, "subplot_index"] = map_altitudes_and_subplots(
                             unit=unit,
                             unique_start_altitudes=unique_altitude_levels_dict[origin],
                             current_altitude=start_df["z"].loc[tmp],
@@ -230,6 +190,7 @@ def read_startf(startf_path, separator):
                         tmp += 1
 
                     i += 4
+
     return start_df
 
 
@@ -309,7 +270,7 @@ def convert_time(plot_info_dict, traj_df, case):
         ):  # hres trajectories have a weird time discretisation where half an hour is discretised as 0.3
             delta_t = delta_t + 0.2
         date = dt_object + timedelta(hours=delta_t)
-        traj_df["datetime"].loc[counter] = date
+        traj_df.loc[counter, "datetime"] = date
 
         counter += 1
 
@@ -326,8 +287,6 @@ def read_trajectory(trajectory_file_path, start_df, plot_info_dict):
 
     Returns:
         traj_df (df):                   Dataframe containig information of trajectory file
-        number_of_trajectories (int):   #trajectories
-        number_of_times (int):          #rows making up one trajectory
 
     """
     # read first line of trajectory file to check which case it is.
@@ -418,25 +377,41 @@ def read_trajectory(trajectory_file_path, start_df, plot_info_dict):
         altitude_levels = start_df["altitude_levels"][tmp]
         subplot_index = int(start_df["subplot_index"][tmp])
         max_start_altitude = start_df["max_start_altitude"][tmp]
-        traj_df["z_type"].iloc[lower_row:upper_row] = z_type
-        traj_df["origin"].iloc[lower_row:upper_row] = origin
-        traj_df["lon_precise"].iloc[lower_row:upper_row] = lon_precise
-        traj_df["lat_precise"].iloc[lower_row:upper_row] = lat_precise
-        traj_df["side_traj"].iloc[lower_row:upper_row] = side_traj
-        traj_df["altitude_levels"].iloc[lower_row:upper_row] = altitude_levels
-        traj_df["start_altitude"].iloc[lower_row:upper_row] = start_altitude
-        traj_df["subplot_index"].iloc[lower_row:upper_row] = subplot_index
-        traj_df["max_start_altitude"].iloc[lower_row:upper_row] = max_start_altitude
+        traj_df.loc[
+            lower_row:upper_row,
+            [
+                "z_type",
+                "origin",
+                "lon_precise",
+                "lat_precise",
+                "side_traj",
+                "altitude_levels",
+                "start_altitude",
+                "subplot_index",
+                "max_start_altitude",
+            ],
+        ] = (
+            z_type,
+            origin,
+            lon_precise,
+            lat_precise,
+            side_traj,
+            altitude_levels,
+            start_altitude,
+            subplot_index,
+            max_start_altitude,
+        )
+
         tmp += 1
 
     traj_df = convert_time(plot_info_dict=plot_info_dict, traj_df=traj_df, case=case)
     traj_df.reset_index(drop=True, inplace=True)
 
     if False:
-        traj_df.to_csv("trajdf.csv", index=True)
-        start_df.to_csv("startf.csv", index=True)
+        traj_df.to_csv("ground_truth_trajdf.csv", index=True)
+        start_df.to_csv("ground_truth_startf.csv", index=True)
 
-    return traj_df, number_of_trajectories, number_of_times
+    return traj_df
 
 
 def check_input_dir(input_dir, prefix_dict, separator):
@@ -455,59 +430,49 @@ def check_input_dir(input_dir, prefix_dict, separator):
 
     """
     print("--- Parsing Input Files")
-    counter = 0
-    start_dict, trajectory_dict, files, keys, traj_blocks = {}, {}, [], [], {}
-
+    start_dict, trajectory_dict, keys = {}, {}, []
     # iterate through the directory, first reading the start & plot_info files (necessary, to parse the trajectory files afterwards)
     for filename in os.listdir(input_dir):
-
         file_path = os.path.join(input_dir, filename)
-        if os.path.isfile(file_path):
-            if filename[: len(prefix_dict["start"])] == prefix_dict["start"]:
-                files.append(filename)
+        if (
+            filename[: len(prefix_dict["start"])] == prefix_dict["start"]
+        ):  # if filename starts w/ start file prefix OR plot_info file name
+            if filename[len(prefix_dict["start"]) :] not in keys:
+                keys.append(filename[len(prefix_dict["start"]) :])
 
-                if filename[len(prefix_dict["start"]) :] not in keys:
-                    keys.append(
-                        filename[len(prefix_dict["start"]) :]
-                    )  # append keys of trajectory_df to a list for later use
+                # check, that there is a matching trajectory file for each start file
+                assert os.path.isfile(
+                    os.path.join(
+                        input_dir,
+                        prefix_dict["trajectory"]
+                        + filename[len(prefix_dict["start"]) :],
+                    )
+                ), f"There is no matching trajectory file for the startfile: {prefix_dict['start']+filename[len(prefix_dict['start']) :]}"
 
-                    traj_blocks[filename[len(prefix_dict["start"]) :]] = {
-                        "number_of_times": 0,
-                        "number_of_trajectories": 0,
-                    }  # create for each start/traj pair a dict, containing the length of a trajectory block
+            start_dict[filename[len(prefix_dict["start"]) :]] = read_startf(
+                startf_path=file_path, separator=separator
+            )
 
-                counter += 1
-                start_dict[filename[len(prefix_dict["start"]) :]] = read_startf(
-                    startf_path=file_path, separator=separator
-                )
+        if filename[: len(prefix_dict["plot_info"])] == prefix_dict["plot_info"]:
+            with open(file_path, "r") as f:
+                plot_info = [line.strip() for line in f.readlines()]
 
-            if filename[: len(prefix_dict["plot_info"])] == prefix_dict["plot_info"]:
-                files.append(filename)
-                plot_info_dict = read_plot_info(plot_info_path=file_path)
+            plot_info_dict = {
+                "mbt": plot_info[1][
+                    44:
+                ],  # model base time (corresponds to the 2nd row; first row is empty)
+                "model_name": plot_info[2][
+                    44:
+                ],  # model name (corresponds to the 3rd row)
+            }
 
-    # iterate through the directory, reading the trajectory and plot_info file after having read the start file
-    for filename in os.listdir(input_dir):
-
-        file_path = os.path.join(input_dir, filename)
-
-        if os.path.isfile(file_path):
-            if filename[: len(prefix_dict["trajectory"])] == prefix_dict["trajectory"]:
-                files.append(filename)
-                (
-                    trajectory_dict[filename[len(prefix_dict["trajectory"]) :]],
-                    number_of_trajectories,
-                    number_of_times,
-                ) = read_trajectory(
-                    trajectory_file_path=file_path,
-                    start_df=start_dict[filename[len(prefix_dict["trajectory"]) :]],
-                    plot_info_dict=plot_info_dict,
-                )
-
-                traj_blocks[filename[len(prefix_dict["trajectory"]) :]][
-                    "number_of_times"
-                ] = number_of_times
-                traj_blocks[filename[len(prefix_dict["trajectory"]) :]][
-                    "number_of_trajectories"
-                ] = number_of_trajectories
+    # for each start file (identified by its unique key) a corresponding trajectory file exists --> parse these afterwards
+    for key in keys:
+        file_path = os.path.join(input_dir, prefix_dict["trajectory"] + key)
+        trajectory_dict[key] = read_trajectory(
+            trajectory_file_path=file_path,
+            start_df=start_dict[key],
+            plot_info_dict=plot_info_dict,
+        )
 
     return trajectory_dict, plot_info_dict, keys
