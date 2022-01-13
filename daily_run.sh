816b588c6e579f2b2fdb6d692901486470bf61da
mizeller <mizeller@student.ethz.ch>
2022-01-13 10:19:34 +0100

    test script to evaluate new data (hres europe/ hres global & cosmo) every morning

---
+++ daily_run.sh
@@ -0,0 +1,5 @@
+# TODO: just change the path to the most recent data
+# (if some other domains or datatypes are missing, add the necessary flags as well)
+pytrajplot /store/mch/msopr/osm/COSMO-1E/FCST22/22011300_407/lagranto_c/000 daily_run/cosmo/ --domain alps --domain ch --datatype png
+pytrajplot /store/mch/msopr/osm/IFS-HRES/IFS-HRES-LAGRANTO22/22011300_LIH/lagranto_f daily_run/hres_lagranto_f/ --domain dynamic --domain dynamic_zoom --datatype png
+pytrajplot /store/mch/msopr/osm/IFS-HRES/IFS-HRES-LAGRANTO22/22011300_LIH/lagranto_c daily_run/hres_lagranto_c/ --domain dynamic --domain europe --domain centraleurope --datatype png
