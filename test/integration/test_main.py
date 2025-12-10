c62269cdda2e29dbdeb4e0382d69c338d111d92c
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2025-12-10 15:44:50 +0100

    Add check for existence of expected output files and successful execution of command.

--- test/integration/test_main.py
+++ test/integration/test_main.py
@@ -6,51 +6,163 @@ import pytest
 
 # First-party
 from pytrajplot import main
+from pathlib import Path
 
+# Arguments and expected outputs for pytrajplot tests
+test_sets = [
 
-hres_test_args = [
-    ['1_altitudes', '1_altitudes', {'datatype': 'png', 'domain': 'europe'}],
-    ['2_altitudes', '2_altitudes', {'datatype': 'png', 'domain': 'europe'}],
-    ['3_altitudes', '3_altitudes', {'datatype': 'png', 'domain': 'europe'}],
-    ['4_altitudes', '5_altitudes', {'datatype': 'png', 'domain': 'europe'}],
+    # HRES tests
+    {
+        'model': 'hres',
+        'arguments': ['1_altitudes', '1_altitudes', {'datatype': 'png', 'domain': 'europe'}],
+        'expected_output': [
+            './test_hres/1_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_europe.png'
+        ]
+    },
+    {
+        'model': 'hres',
+        'arguments': ['2_altitudes', '2_altitudes', {'datatype': 'png', 'domain': 'europe'}],
+        'expected_output': [
+            './test_hres/2_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_europe.png'
+        ]
+    },
+    {
+        'model': 'hres',
+        'arguments': ['3_altitudes', '3_altitudes', {'datatype': 'png', 'domain': 'europe'}],
+        'expected_output': [
+            './test_hres/3_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_europe.png'
+        ]
+    },
 
     # backward
-    ['backward', 'backward', {'datatype': 'png', 'domain': ['europe','dynamic']}],
+    {
+        'model': 'hres',
+        'arguments': ['backward', 'backward', {'datatype': 'png', 'domain': ['europe', 'dynamic']}],
+        'expected_output': [
+            './test_hres/backward/20210505T00_Geneve_LAGRANTO-IFS-HRES-Europe_Trajektorien_B_036_europe.png'
+        ]
+    },
 
     # all domains combined
-    ['4_altitudes', '4_altitudes', {
-        'datatype': 'png',
-        'domain': ['ch', 'alps', 'centraleurope', 'europe', 'dynamic']
-    }],
+    {
+        'model': 'hres',
+        'arguments': ['4_altitudes', '4_altitudes', {
+            'datatype': 'png',
+            'domain': ['ch', 'alps', 'centraleurope', 'europe', 'dynamic']
+        }],
+        'expected_output': [
+            './test_hres/4_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_centraleurope.png',
+            './test_hres/4_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_alps.png',
+            './test_hres/4_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_dynamic.png',
+            './test_hres/4_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_ch.png',
+            './test_hres/4_altitudes/20210503T12_Linate_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_048_europe.png'
+        ]
+    },
 
     # dateline crossing
-    ['dateline', 'dateline', {'datatype': 'png', 'domain': 'dynamic'}],
+    {
+        'model': 'hres',
+        'arguments': ['dateline', 'dateline', {'datatype': 'png', 'domain': 'dynamic'}],
+        'expected_output': [
+            './test_hres/dateline/20210503T18_Punggye-ri_LAGRANTO-IFS-HRES_Trajektorien_F_138_dynamic.png'
+        ]
+    },
 
     # german case (dateline + language)
-    ['dateline', 'dateline/german', {
-        'datatype': 'png',
-        'domain': 'dynamic',
-        'language': 'de'
-    }],
+    {
+        'model': 'hres',
+        'arguments': ['dateline', 'dateline/german', {
+            'datatype': 'png',
+            'domain': 'dynamic',
+            'language': 'de'
+        }],
+        'expected_output': [
+            './test_hres/dateline/german/20210503T18_Punggye-ri_LAGRANTO-IFS-HRES_Trajektorien_F_138_dynamic.png'
+        ]
+    },
 
     # zero longitude from east
-    ['zero_lon_from_east', 'zero_lon_from_east', {}],
+    {
+        'model': 'hres',
+        'arguments': ['zero_lon_from_east', 'zero_lon_from_east', {}],
+        'expected_output': [
+            './test_hres/zero_lon_from_east/20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
+            './test_hres/zero_lon_from_east/20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf'
+        ]
+    },
 
     # zero last longitude
-    ['zero_last_lon', 'zero_last_lon', {}],
+    {
+        'model': 'hres',
+        'arguments': ['zero_last_lon', 'zero_last_lon', {}],
+        'expected_output': [
+            './test_hres/zero_last_lon/20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
+            './test_hres/zero_last_lon/20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
+            './test_hres/zero_last_lon/20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
+            './test_hres/zero_last_lon/20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
+            './test_hres/zero_last_lon/20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
+            './test_hres/zero_last_lon/20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf'
+        ]
+    },
 
     # zero lon + dateline crossing
-    ['zero_lon_dateline', 'zero_lon_dateline', {
-        'datatype': 'png',
-        'domain': 'dynamic'
-    }],
-]
+    {
+        'model': 'hres',
+        'arguments': ['zero_lon_dateline', 'zero_lon_dateline', {
+            'datatype': 'png',
+            'domain': 'dynamic'
+        }],
+        'expected_output': [
+            './test_hres/zero_lon_dateline/20240913T00_Lugano_LAGRANTO-IFS-HRES_Trajektorien_B_144_dynamic.png'
+        ]
+    },
 
-
-
-cosmo_test_args = [
-    ['forward', 'forward', {'datatype': 'png', 'domain': ['ch','alps']}],
-    ['backward', 'backward', {'datatype': 'png', 'domain': ['ch','alps']}]
+    # COSMO tests
+    {
+        'model': 'cosmo',
+        'arguments': ['forward', 'forward', {'datatype': 'png', 'domain': ['ch', 'alps']}],
+        'expected_output': [
+            './test_cosmo/forward/20211011T00_Geneve_LAGRANTO-COSMO-1E_Trajektorien_F_033_ch.png',
+            './test_cosmo/forward/20211011T00_Geneve_LAGRANTO-COSMO-1E_Trajektorien_F_033_alps.png'
+        ]
+    },
+    {
+        'model': 'cosmo',
+        'arguments': ['backward', 'backward', {'datatype': 'png', 'domain': ['ch', 'alps']}],
+        'expected_output': [
+            './test_cosmo/backward/20211103T09_Geneve_LAGRANTO-COSMO-1E_Trajektorien_B_033_alps.png',
+            './test_cosmo/backward/20211103T09_Geneve_LAGRANTO-COSMO-1E_Trajektorien_B_033_ch.png'
+        ]
+    }
 ]
 
 def create_args(input_dir: str, output_dir: str, opts: dict) -> list:
@@ -77,24 +189,23 @@ def create_args(input_dir: str, output_dir: str, opts: dict) -> list:
             args.append(cli_key)
     return args
 
-@pytest.mark.parametrize("input_args", hres_test_args)
-def test_hres(input_args, hres_input, output_dir):
-    in_pathname, out_pathname, opts = input_args
+@pytest.mark.parametrize("input_args", test_sets)
+def test_pytrajplot(input_args, input_dir, output_dir):
+    in_pathname, out_pathname, opts = input_args['arguments']
+    expected = input_args.get('expected_output', [])
+    model = input_args.get('model')
 
-    input_dir = str(hres_input / in_pathname)
-    output_dir = str(output_dir / 'hres' / out_pathname)
+    # compute source input and output paths
+    input_path = str(input_dir / in_pathname)
+    output_path = str(output_dir / model / out_pathname)
 
-    args = create_args(input_dir, output_dir, opts)
+    args = create_args(input_path, output_path, opts)
     runner = CliRunner()
-    runner.invoke(main.cli, args)
+    result = runner.invoke(main.cli, args)
+    assert result.exit_code == 0, f"CLI exited with non-zero status: {result.exit_code}\nOutput:\n{result.output}"
 
-@pytest.mark.parametrize("input_args", cosmo_test_args)
-def test_cosmo(input_args, cosmo_input, output_dir):
-    in_pathname, out_pathname, opts = input_args
+    # verify expected output files were created
+    for rel in expected:
+        expected_file = Path(output_path) / Path(rel).name
+        assert expected_file.exists(), f"Expected output not found: {expected_file}"
 
-    input_dir = str(cosmo_input / in_pathname)
-    output_dir = str(output_dir / 'cosmo' / out_pathname)
-
-    args = create_args(input_dir, output_dir, opts)
-    runner = CliRunner()
-    runner.invoke(main.cli, args)
