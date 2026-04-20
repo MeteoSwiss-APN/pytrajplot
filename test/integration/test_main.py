dc4a7927ceb64a559e9f96d79ff0695966489eaf
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2026-04-20 16:59:11 +0200

    Update test cases for integration tests

--- test/integration/test_main.py
+++ test/integration/test_main.py
@@ -11,158 +11,202 @@ from pathlib import Path
 # Arguments and expected outputs for pytrajplot tests
 test_sets = [
 
-    # HRES tests
+    # ICON-CH1-EPS tests
     {
-        'model': 'hres',
+        'model': 'icon-ch1-eps',
+        'arguments': ['backward_033-000', 'backward_033-000', {'datatype': 'png', 'domain': ['ch', 'alps']}],
+        'expected_output': [
+            'forecast-iconch1eps-trajectories~20260414T12~backward~alps~Geneve~20260415T21.png',
+            'forecast-iconch1eps-trajectories~20260414T12~backward~ch~Geneve~20260415T21.png',
+        ]
+    },
+    {
+        'model': 'icon-ch1-eps',
+        'arguments': ['forward_000-033', 'forward_000-033', {'datatype': 'png', 'domain': ['ch', 'alps']}],
+        'expected_output': [
+            'forecast-iconch1eps-trajectories~20260414T12~forward~alps~Geneve~20260414T12.png',
+            'forecast-iconch1eps-trajectories~20260414T12~forward~ch~Geneve~20260414T12.png',
+        ]
+    },
+    {
+        'model': 'icon-ch1-eps',
+        'arguments': ['forward_003-045', 'forward_003-045', {'datatype': 'png', 'domain': ['ch', 'alps']}],
+        'expected_output': [
+            'forecast-iconch1eps-trajectories~20260414T03~forward~alps~Beznau~20260414T06.png',
+            'forecast-iconch1eps-trajectories~20260414T03~forward~alps~Goesgen~20260414T06.png',
+            'forecast-iconch1eps-trajectories~20260414T03~forward~alps~Leibstadt~20260414T06.png',
+            'forecast-iconch1eps-trajectories~20260414T03~forward~ch~Beznau~20260414T06.png',
+            'forecast-iconch1eps-trajectories~20260414T03~forward~ch~Goesgen~20260414T06.png',
+            'forecast-iconch1eps-trajectories~20260414T03~forward~ch~Leibstadt~20260414T06.png',
+        ]
+    },
+
+    # IFS-Europe tests
+    {
+        'model': 'ifs-europe',
+        'arguments': ['backward_072-000', 'backward_072-000', {'datatype': 'png', 'domain': ['alps', 'centraleurope', 'europe']}],
+        'expected_output': [
+            'forecast-ifseurope-trajectories~20260414T12~backward~alps~Geneve~20260417T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~backward~alps~Jungfraujoch~20260417T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~backward~centraleurope~Geneve~20260417T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~backward~centraleurope~Jungfraujoch~20260417T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~backward~europe~Geneve~20260417T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~backward~europe~Jungfraujoch~20260417T12.png',
+        ]
+    },
+    {
+        'model': 'ifs-europe',
+        'arguments': ['forward_000-048', 'forward_000-048', {'datatype': 'png', 'domain': ['alps', 'centraleurope', 'europe']}],
+        'expected_output': [
+            'forecast-ifseurope-trajectories~20260414T12~forward~alps~Attisholz~20260414T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~forward~alps~Chateau_dOex~20260414T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~forward~centraleurope~Attisholz~20260414T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~forward~centraleurope~Chateau_dOex~20260414T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~forward~europe~Attisholz~20260414T12.png',
+            'forecast-ifseurope-trajectories~20260414T12~forward~europe~Chateau_dOex~20260414T12.png',
+        ]
+    },
+    {
+        'model': 'ifs-europe',
+        'arguments': ['forward_006-090', 'forward_006-090', {'datatype': 'png', 'domain': ['alps', 'centraleurope', 'europe']}],
+        'expected_output': [
+            'forecast-ifseurope-trajectories~20260414T12~forward~alps~Chernobyl~20260414T18.png',
+            'forecast-ifseurope-trajectories~20260414T12~forward~centraleurope~Chernobyl~20260414T18.png',
+            'forecast-ifseurope-trajectories~20260414T12~forward~europe~Chernobyl~20260414T18.png',
+        ]
+    },
+
+    # IFS-Global tests
+    {
+        'model': 'ifs-global',
+        'arguments': ['backward_144-000', 'backward_144-000', {'datatype': 'png', 'domain': ['dynamic', 'dynamic_zoom']}],
+        'expected_output': [
+            'forecast-ifsglobal-trajectories~20260414T12~backward~dynamic~Geneve~20260420T12.png',
+            'forecast-ifsglobal-trajectories~20260414T12~backward~dynamic~Jungfraujoch~20260420T12.png',
+            'forecast-ifsglobal-trajectories~20260414T12~backward~dynamic_zoom~Geneve~20260420T12.png',
+            'forecast-ifsglobal-trajectories~20260414T12~backward~dynamic_zoom~Jungfraujoch~20260420T12.png',
+        ]
+    },
+    {
+        'model': 'ifs-global',
+        'arguments': ['forward_000-144', 'forward_000-144', {'datatype': 'png', 'domain': ['dynamic', 'dynamic_zoom']}],
+        'expected_output': [
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic~Bushehr~20260414T12.png',
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic~Punggye-ri~20260414T12.png',
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic_zoom~Bushehr~20260414T12.png',
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic_zoom~Punggye-ri~20260414T12.png'
+        ]
+    },
+    {
+        'model': 'ifs-global',
+        'arguments': ['forward_006-144', 'forward_006-144', {'datatype': 'png', 'domain': ['dynamic', 'dynamic_zoom']}],
+        'expected_output': [
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic~Bushehr~20260414T18.png',
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic~Punggye-ri~20260414T18.png',
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic_zoom~Bushehr~20260414T18.png',
+            'forecast-ifsglobal-trajectories~20260414T12~forward~dynamic_zoom~Punggye-ri~20260414T18.png',
+        ]
+    },
+
+    # IFS test for special cases
+    # 1, 2, 3, altitudes
+    {
+        'model': 'ifs-testcases',
         'arguments': ['1_altitudes', '1_altitudes', {'datatype': 'png', 'domain': 'europe'}],
         'expected_output': [
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
+            'forecast-ifseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
         ]
     },
     {
-        'model': 'hres',
+        'model': 'ifs-testcases',
         'arguments': ['2_altitudes', '2_altitudes', {'datatype': 'png', 'domain': 'europe'}],
         'expected_output': [
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
+            'forecast-ifseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
         ]
     },
     {
-        'model': 'hres',
+        'model': 'ifs-testcases',
         'arguments': ['3_altitudes', '3_altitudes', {'datatype': 'png', 'domain': 'europe'}],
         'expected_output': [
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
+            'forecast-ifseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
         ]
     },
 
-    # backward
+    # 4 altitudes, all domains
     {
-        'model': 'hres',
-        'arguments': ['backward', 'backward', {'datatype': 'png', 'domain': ['europe', 'dynamic']}],
-        'expected_output': [
-            'forecast-ifshreseurope-trajectories~20210505T00~backward~europe~Geneve~20210506T12.png'
-        ]
-    },
-
-    # all domains combined
-    {
-        'model': 'hres',
+        'model': 'ifs-testcases',
         'arguments': ['4_altitudes', '4_altitudes', {
             'datatype': 'png',
             'domain': ['ch', 'alps', 'centraleurope', 'europe', 'dynamic']
         }],
         'expected_output': [
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~centraleurope~Linate~20210503T12.png',
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~alps~Linate~20210503T12.png',
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~dynamic~Linate~20210503T12.png',
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~ch~Linate~20210503T12.png',
-            'forecast-ifshreseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
+            'forecast-ifseurope-trajectories~20210503T12~forward~alps~Linate~20210503T12.png',
+            'forecast-ifseurope-trajectories~20210503T12~forward~centraleurope~Linate~20210503T12.png',
+            'forecast-ifseurope-trajectories~20210503T12~forward~ch~Linate~20210503T12.png',
+            'forecast-ifseurope-trajectories~20210503T12~forward~dynamic~Linate~20210503T12.png',
+            'forecast-ifseurope-trajectories~20210503T12~forward~europe~Linate~20210503T12.png'
         ]
     },
 
-    # dateline crossing
+    # basetime after reftime
     {
-        'model': 'hres',
-        'arguments': ['dateline', 'dateline', {'datatype': 'png', 'domain': 'dynamic'}],
+        'model': 'ifs-testcases',
+        'arguments': ['basetime_after_reftime', 'basetime_after_reftime', {
+            'datatype': 'png',
+            'domain': 'dynamic'
+        }],
         'expected_output': [
-            'forecast-ifshres-trajectories~20210503T18~forward~dynamic~Punggye-ri~20210504T00.png'
+            'forecast-ifseurope-trajectories~20210503T18~forward~dynamic~Linate~20210503T12.png'
         ]
     },
 
-    # german case (dateline + language)
+    # dateline crossing, german annotations (dateline + language)
     {
-        'model': 'hres',
-        'arguments': ['dateline', 'dateline/german', {
+        'model': 'ifs-testcases',
+        'arguments': ['dateline', 'dateline', {
             'datatype': 'png',
             'domain': 'dynamic',
             'language': 'de'
         }],
         'expected_output': [
-            'forecast-ifshres-trajectories~20210503T18~forward~dynamic~Punggye-ri~20210504T00.png'
+            'forecast-ifsglobal-trajectories~20210503T12~forward~dynamic~Punggye-ri~20210503T18.png'
         ]
     },
 
-    # zero longitude from east
+    # dateline crossing from west
     {
-        'model': 'hres',
-        'arguments': ['zero_lon_from_east', 'zero_lon_from_east', {}],
+        'model': 'ifs-testcases',
+        'arguments': ['dateline_from_west', 'dateline_from_west', {
+            'datatype': 'png',
+            'domain': 'dynamic',
+            'language': 'de'
+        }],
         'expected_output': [
-            '20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
-            '20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
-            '20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
-            '20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
-            '20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
-            '20220401T00_Chernobyl_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
-            '20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
-            '20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
-            '20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
-            '20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
-            '20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
-            '20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
-            '20220401T00_Chmelnyzkyj_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
-            '20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
-            '20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
-            '20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
-            '20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
-            '20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
-            '20220401T00_Riwne_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
-            '20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
-            '20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
-            '20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
-            '20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
-            '20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
-            '20220401T00_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
-            '20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
-            '20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
-            '20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
-            '20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
-            '20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf',
-            '20220401T00_Zaporozhye_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf'
+            'forecast-ifsglobal-trajectories~20211223T00~backward~dynamic~Geneve~20211229T00.png'
         ]
     },
 
     # zero last longitude
     {
-        'model': 'hres',
-        'arguments': ['zero_last_lon', 'zero_last_lon', {}],
+        'model': 'ifs-testcases',
+        'arguments': ['zero_last_lon', 'zero_last_lon', {'datatype': 'png', 'domain': 'europe'}],
         'expected_output': [
-            '20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic.pdf',
-            '20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_ch.pdf',
-            '20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_alps.pdf',
-            '20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_centraleurope.pdf',
-            '20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_europe.pdf',
-            '20230205T12_Sued-Ukraine_LAGRANTO-IFS-HRES-Europe_Trajektorien_F_084_dynamic_zoom.pdf'
+            'forecast-ifseurope-trajectories~20230205T06~forward~europe~Sued-Ukraine~20230205T12.png'
         ]
     },
 
     # zero lon + dateline crossing
     {
-        'model': 'hres',
+        'model': 'ifs-testcases',
         'arguments': ['zero_lon_dateline', 'zero_lon_dateline', {
             'datatype': 'png',
             'domain': 'dynamic'
         }],
         'expected_output': [
-            'forecast-ifshres-trajectories~20240913T00~backward~dynamic~Lugano~20240919T00.png'
+            'forecast-ifsglobal-trajectories~20240907T00~backward~dynamic~Lugano~20240913T00.png'
         ]
     },
 
-    # COSMO tests
-    {
-        'model': 'cosmo',
-        'arguments': ['forward', 'forward', {'datatype': 'png', 'domain': ['ch', 'alps']}],
-        'expected_output': [
-            'forecast-cosmo1e-trajectories~20211011T00~forward~ch~Geneve~20211011T00.png',
-            'forecast-cosmo1e-trajectories~20211011T00~forward~alps~Geneve~20211011T00.png'
-        ]
-    },
-    {
-        'model': 'cosmo',
-        'arguments': ['backward', 'backward', {'datatype': 'png', 'domain': ['ch', 'alps']}],
-        'expected_output': [
-            'forecast-cosmo1e-trajectories~20211103T09~backward~alps~Geneve~20211104T18.png',
-            'forecast-cosmo1e-trajectories~20211103T09~backward~ch~Geneve~20211104T18.png'
-        ]
-    }
 ]
 
 def create_args(input_dir: str, output_dir: str, opts: dict) -> list:
